c1d09a395d3f48dde56eee8438b2ad93d5b0a3bc
Alex Castelo <alexcastelo437@gmail.com>
2023-01-05 20:13:40 -0300

    feat: created patientRepository

---
+++ src/main/java/com/pimpos/patientmanagement/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.pimpos.patientmanagement.repository;
+
+import com.pimpos.patientmanagement.model.PatientModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<PatientModel, UUID> {
+}
