3e8655d3c858e99a3caf4ab6d71acc7ce69e026d
Alex Castelo <alexcastelo437@gmail.com>
2023-01-05 20:12:45 -0300

    feat: created patientModel

---
+++ src/main/java/com/pimpos/patientmanagement/model/PatientModel.java
@@ -0,0 +1,82 @@
+package com.pimpos.patientmanagement.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "patients")
+public class PatientModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String age;
+    @Column(nullable = false, unique = true, length = 14)
+    private String cpf;
+    @Column(nullable = false)
+    private LocalDate birthdate;
+    @Column(nullable = false)
+    private String contact;
+    @Column(nullable = false)
+    private String address;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
