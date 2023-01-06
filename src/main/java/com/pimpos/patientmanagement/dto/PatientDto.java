73c412178683c5a0d7aa119410c02ede22afbfa2
Alex Castelo <alexcastelo437@gmail.com>
2023-01-06 11:05:54 -0300

    feat: created patientDto

---
+++ src/main/java/com/pimpos/patientmanagement/dto/PatientDto.java
@@ -0,0 +1,72 @@
+package com.pimpos.patientmanagement.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+public class PatientDto {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String age;
+    @NotBlank
+    @Size(min = 14, max = 14)
+    @Pattern(regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$")
+    private String cpf;
+    @NotNull
+    private LocalDate birthdate;
+    @NotBlank
+    private String contact;
+    @NotBlank
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
