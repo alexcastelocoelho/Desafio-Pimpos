4931375e81d9321f53c0b7648ccdebd1887748a0
Alex Castelo <alexcastelo437@gmail.com>
2023-01-06 21:33:55 -0300

    feat: created responsiblePatientDto

---
+++ src/main/java/com/pimpos/patientmanagement/dto/ResponsiblePatientDto.java
@@ -0,0 +1,73 @@
+package com.pimpos.patientmanagement.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public class ResponsiblePatientDto {
+    @NotBlank
+    private String name;
+    @NotBlank
+    @Size(min = 14, max = 14)
+    @Pattern(regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$")
+    private String cpf;
+    @NotNull
+    private LocalDate birthdate;
+    @NotBlank
+    private String kinship;
+    @NotBlank
+    private String contact;
+    @NotBlank
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getKinship() {
+        return kinship;
+    }
+
+    public void setKinship(String kinship) {
+        this.kinship = kinship;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
