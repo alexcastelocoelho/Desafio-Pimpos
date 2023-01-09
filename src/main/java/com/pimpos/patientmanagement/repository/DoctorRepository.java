06846bd52911030ab2852ceae1923ef8aed47dd7
Alex Castelo <alexcastelo437@gmail.com>
2023-01-09 09:23:18 -0300

    feat: created doctorRepository

---
+++ src/main/java/com/pimpos/patientmanagement/repository/DoctorRepository.java
@@ -0,0 +1,11 @@
+package com.pimpos.patientmanagement.repository;
+
+import com.pimpos.patientmanagement.model.DoctorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface DoctorRepository extends JpaRepository<DoctorModel, UUID> {
+
+}
