74e1a01a136a0aad63c71f13e8f489bf8c38ff19
Alex Castelo <alexcastelo437@gmail.com>
2023-01-18 10:21:26 -0300

    feat: added validations for doctor

--- src/main/java/com/pimpos/patientmanagement/dto/DoctorDto.java
+++ src/main/java/com/pimpos/patientmanagement/dto/DoctorDto.java
@@ -1,6 +1,7 @@
 package com.pimpos.patientmanagement.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -8,15 +9,16 @@ import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public class DoctorDto {
-    @NotBlank
+    @NotBlank(message = "enter your name")
+    @Size(min = 3, max = 40, message = "your name must have at least three letters")
     private String name;
-    @NotBlank
+    @NotBlank(message = "provide your council number")
     private String adviceNumber;
     @NotBlank
     @Size(min = 14, max = 14)
     @Pattern(regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$")
     private String cpf;
-    @NotBlank
+    @NotBlank(message = "inform your address")
     private String address;
 
     public String getName() {
